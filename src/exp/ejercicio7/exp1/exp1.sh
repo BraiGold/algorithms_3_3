1f434213362234409fc85e1ec2854d93073f80b0
Lucía Rabinowicz <lu.rabinowicz@gmail.com>
2016-07-13 13:30:43 -0300

    borro cosas y cambio un .sh

--- src/exp/ejercicio7/exp1/exp1.sh
+++ src/exp/ejercicio7/exp1/exp1.sh
@@ -26,115 +26,115 @@ while getopts 'ha:' opt; do
 done
 
 
-# #genero archivos de entrada 
+#genero archivos de entrada 
 
-# for i in $n1; do
-#   m1=$((3*i))
-#   echo "Esta creando el archivo numero "
-#   printf "%d\n " $i
-#   printf "f3 %d %d %d %d \n" $i $m1 $n2 $m2 | $(dirname $0)/../../../generador-grafo-rapido 
-# done
+for i in $n1; do
+  m1=$((3*i))
+  echo "Esta creando el archivo numero "
+  printf "%d\n " $i
+  printf "f3 %d %d %d %d \n" $i $m1 $n2 $m2 | $(dirname $0)/../../../generador-grafo-rapido 
+done
 
-# for i in $n1; do
-#   aux=$((i^2))
-#   m1=$((aux/10))
-#   echo "Esta creando el archivo numero "
-#   printf "%d\n " $i
-#   printf "f4 %d %d %d %d \n" $i $m1 $n2 $m2 | $(dirname $0)/../../../generador-grafo-rapido 
-# done
+for i in $n1; do
+  aux=$((i^2))
+  m1=$((aux/10))
+  echo "Esta creando el archivo numero "
+  printf "%d\n " $i
+  printf "f4 %d %d %d %d \n" $i $m1 $n2 $m2 | $(dirname $0)/../../../generador-grafo-rapido 
+done
 
-# #==============================tipo1=tiempos=======================================================
+#==============================tipo1=tiempos=======================================================
 
-# printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej4-exp1-f3.txt
-# printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej5-exp1-f3-tipo1.txt
-# printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej6-exp1-f3-tipo1.txt
-
-
-#   for k in $n1; do
-#     printf "%d " $k >> $(dirname $0)/tiempos-ej4-exp1-f3.txt
-#     m1=$((3*k))
-#     echo "Esta corriendo la instancia numero"
-#     printf "%d\n " $k
-#     for h in $(seq 1 $iteraciones); do
-#       echo "iteracion numero"
-#       printf "%d\n " $h
-#       $(dirname $0)/../../../ejercicio4 < $(dirname $0)/f3/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt -t >> $(dirname $0)/tiempos-ej4-exp1-f3.txt
-#     done
-#     printf "\n" >> $(dirname $0)/tiempos-ej4-exp1-f3.txt
-#   done
-
-#   for k in $n1; do
-#     printf "%d " $k >> $(dirname $0)/tiempos-ej5-exp1-f3-tipo1.txt
-#     m1=$((3*k))
-#     echo "Esta corriendo la instancia numero"
-#     printf "%d\n " $k
-#     for h in $(seq 1 $iteraciones); do
-#       echo "iteracion numero"
-#       printf "%d\n " $h
-#       $(dirname $0)/../../../ejercicio5-tipo1 < $(dirname $0)/f3/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt $cuantosMiro -t >> $(dirname $0)/tiempos-ej5-exp1-f3-tipo1.txt
-#     done
-#     printf "\n" >> $(dirname $0)/tiempos-ej5-exp1-f3-tipo1.txt
-#   done
-
-#   for k in $n1; do
-#     printf "%d " $k >> $(dirname $0)/tiempos-ej6-exp1-f3-tipo1.txt
-#     m1=$((3*k))
-#     echo "Esta corriendo la instancia numero"
-#     printf "%d\n " $k
-#     for h in $(seq 1 $iteraciones); do
-#       echo "iteracion numero"
-#       printf "%d\n " $h
-#       $(dirname $0)/../../../ejercicio6-tipo1 < $(dirname $0)/f3/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt $cuantosMiro $tamTabu $z -t >> $(dirname $0)/tiempos-ej6-exp1-f3-tipo1.txt
-#     done
-#     printf "\n" >> $(dirname $0)/tiempos-ej6-exp1-f3-tipo1.txt
-#   done
-
-# printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej4-exp1-f4.txt
-# printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej5-exp1-f4-tipo1.txt
-# printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej6-exp1-f4-tipo1.txt
+printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej4-exp1-f3.txt
+printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej5-exp1-f3-tipo1.txt
+printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej6-exp1-f3-tipo1.txt
 
 
-#   for k in $n1; do
-#     printf "%d " $k >> $(dirname $0)/tiempos-ej4-exp1-f4.txt
-#     aux=$((k^2))
-#     m1=$((aux/10))
-#     echo "Esta corriendo la instancia numero"
-#     printf "%d\n " $k
-#     for h in $(seq 1 $iteraciones); do
-#       echo "iteracion numero"
-#       printf "%d\n " $h
-#       $(dirname $0)/../../../ejercicio4 < $(dirname $0)/f4/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt -t >> $(dirname $0)/tiempos-ej4-exp1-f4.txt
-#     done
-#     printf "\n" >> $(dirname $0)/tiempos-ej4-exp1-f4.txt
-#   done
+  for k in $n1; do
+    printf "%d " $k >> $(dirname $0)/tiempos-ej4-exp1-f3.txt
+    m1=$((3*k))
+    echo "Esta corriendo la instancia numero"
+    printf "%d\n " $k
+    for h in $(seq 1 $iteraciones); do
+      echo "iteracion numero"
+      printf "%d\n " $h
+      $(dirname $0)/../../../ejercicio4 < $(dirname $0)/f3/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt -t >> $(dirname $0)/tiempos-ej4-exp1-f3.txt
+    done
+    printf "\n" >> $(dirname $0)/tiempos-ej4-exp1-f3.txt
+  done
 
-#   for k in $n1; do
-#     printf "%d " $k >> $(dirname $0)/tiempos-ej5-exp1-f4-tipo1.txt
-#     aux=$((k^2))
-#     m1=$((aux/10))
-#     echo "Esta corriendo la instancia numero"
-#     printf "%d\n " $k
-#     for h in $(seq 1 $iteraciones); do
-#       echo "iteracion numero"
-#       printf "%d\n " $h
-#       $(dirname $0)/../../../ejercicio5-tipo1 < $(dirname $0)/f4/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt $cuantosMiro -t >> $(dirname $0)/tiempos-ej5-exp1-f4-tipo1.txt
-#     done
-#     printf "\n" >> $(dirname $0)/tiempos-ej5-exp1-f4-tipo1.txt
-#   done
+  for k in $n1; do
+    printf "%d " $k >> $(dirname $0)/tiempos-ej5-exp1-f3-tipo1.txt
+    m1=$((3*k))
+    echo "Esta corriendo la instancia numero"
+    printf "%d\n " $k
+    for h in $(seq 1 $iteraciones); do
+      echo "iteracion numero"
+      printf "%d\n " $h
+      $(dirname $0)/../../../ejercicio5-tipo1 < $(dirname $0)/f3/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt $cuantosMiro -t >> $(dirname $0)/tiempos-ej5-exp1-f3-tipo1.txt
+    done
+    printf "\n" >> $(dirname $0)/tiempos-ej5-exp1-f3-tipo1.txt
+  done
 
-#   for k in $n1; do
-#     printf "%d " $k >> $(dirname $0)/tiempos-ej6-exp1-f4-tipo1.txt
-#     aux=$((k^2))
-#     m1=$((aux/10))
-#     echo "Esta corriendo la instancia numero"
-#     printf "%d\n " $k
-#     for h in $(seq 1 $iteraciones); do
-#       echo "iteracion numero"
-#       printf "%d\n " $h
-#       $(dirname $0)/../../../ejercicio6-tipo1 < $(dirname $0)/f4/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt $cuantosMiro $tamTabu $z -t >> $(dirname $0)/tiempos-ej6-exp1-f4-tipo1.txt
-#     done
-#     printf "\n" >> $(dirname $0)/tiempos-ej6-exp1-f4-tipo1.txt
-#   done
+  for k in $n1; do
+    printf "%d " $k >> $(dirname $0)/tiempos-ej6-exp1-f3-tipo1.txt
+    m1=$((3*k))
+    echo "Esta corriendo la instancia numero"
+    printf "%d\n " $k
+    for h in $(seq 1 $iteraciones); do
+      echo "iteracion numero"
+      printf "%d\n " $h
+      $(dirname $0)/../../../ejercicio6-tipo1 < $(dirname $0)/f3/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt $cuantosMiro $tamTabu $z -t >> $(dirname $0)/tiempos-ej6-exp1-f3-tipo1.txt
+    done
+    printf "\n" >> $(dirname $0)/tiempos-ej6-exp1-f3-tipo1.txt
+  done
+
+printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej4-exp1-f4.txt
+printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej5-exp1-f4-tipo1.txt
+printf "%d \n" $iteraciones >> $(dirname $0)/tiempos-ej6-exp1-f4-tipo1.txt
+
+
+  for k in $n1; do
+    printf "%d " $k >> $(dirname $0)/tiempos-ej4-exp1-f4.txt
+    aux=$((k^2))
+    m1=$((aux/10))
+    echo "Esta corriendo la instancia numero"
+    printf "%d\n " $k
+    for h in $(seq 1 $iteraciones); do
+      echo "iteracion numero"
+      printf "%d\n " $h
+      $(dirname $0)/../../../ejercicio4 < $(dirname $0)/f4/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt -t >> $(dirname $0)/tiempos-ej4-exp1-f4.txt
+    done
+    printf "\n" >> $(dirname $0)/tiempos-ej4-exp1-f4.txt
+  done
+
+  for k in $n1; do
+    printf "%d " $k >> $(dirname $0)/tiempos-ej5-exp1-f4-tipo1.txt
+    aux=$((k^2))
+    m1=$((aux/10))
+    echo "Esta corriendo la instancia numero"
+    printf "%d\n " $k
+    for h in $(seq 1 $iteraciones); do
+      echo "iteracion numero"
+      printf "%d\n " $h
+      $(dirname $0)/../../../ejercicio5-tipo1 < $(dirname $0)/f4/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt $cuantosMiro -t >> $(dirname $0)/tiempos-ej5-exp1-f4-tipo1.txt
+    done
+    printf "\n" >> $(dirname $0)/tiempos-ej5-exp1-f4-tipo1.txt
+  done
+
+  for k in $n1; do
+    printf "%d " $k >> $(dirname $0)/tiempos-ej6-exp1-f4-tipo1.txt
+    aux=$((k^2))
+    m1=$((aux/10))
+    echo "Esta corriendo la instancia numero"
+    printf "%d\n " $k
+    for h in $(seq 1 $iteraciones); do
+      echo "iteracion numero"
+      printf "%d\n " $h
+      $(dirname $0)/../../../ejercicio6-tipo1 < $(dirname $0)/f4/grafo-n1-$k-m1-$m1-n2-$n2-m2-$m2.txt $cuantosMiro $tamTabu $z -t >> $(dirname $0)/tiempos-ej6-exp1-f4-tipo1.txt
+    done
+    printf "\n" >> $(dirname $0)/tiempos-ej6-exp1-f4-tipo1.txt
+  done
 
 #==============================tipo1=aristas=======================================================
 
